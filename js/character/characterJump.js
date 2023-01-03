b3e40e328cbb77acde68eba375a5aca620c363e8
Matthew Powell <matt.powell2784@gmail.com>
2023-01-03 19:46:51 +0000

    jump logic updated

--- js/character/characterJump.js
+++ js/character/characterJump.js
@@ -5,22 +5,12 @@ import { SPEED, DELTA } from '../globalVariables.js'
 export const characterJump = () => {
   document.removeEventListener('keydown', jumnpOnKeyDown)
   const character = document.getElementById('main__char')
-  const bottomPosition = '23.5%'
 
-  const characterCurrentBottomPosition = getPropertyValue(character, 'bottom')
-  console.log('characterCurrentBottomPosition', characterCurrentBottomPosition)
-  const characterNewBottomPosition = `${characterCurrentBottomPosition + 20}px`
+  const jumpTime = SPEED + 1 / DELTA
+  setPropertyValue(character, 'animation', `jump ${jumpTime}s`)
 
-  const jumpTime = DELTA * SPEED - 1
-  const jumpTimePositive = jumpTime * -1
-  console.log('jumpTime', jumpTime)
-  console.log('jumpTimePositive', jumpTimePositive)
-
-  character.style.setProperty('animation', `jump ${jumpTimePositive}s`)
   setTimeout(() => {
     document.addEventListener('keydown', jumnpOnKeyDown)
     character.style.setProperty('animation', 'unset')
-  }, 1000)
-
-  //setPropertyValue(character, 'animation', 'jump 1s')
+  }, jumpTime * 500)
 }
--- js/globalVariables.js
+++ js/globalVariables.js
@@ -1,4 +1,4 @@
-export const SPEED = 0.1
+export const SPEED = 0.5
 export let DELTA
 export const setGlobalDelta = (delta) => {
   DELTA = delta
