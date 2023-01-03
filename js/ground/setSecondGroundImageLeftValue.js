28e3bfd9fdb9ff9676b1190798f54ad64e062a4d
Matthew Powell <matt.powell2784@gmail.com>
2023-01-03 17:44:04 +0000

    pause game on window resize event listener added

--- js/ground/setSecondGroundImageLeftValue.js
+++ js/ground/setSecondGroundImageLeftValue.js
@@ -3,7 +3,6 @@ import { getGroundConatinerWidth } from './getGroundContainerWidth.js'
 import { getPropertyValue, setPropertyValue } from '../helpers/updateProperties.js'
 
 export const setSecondGroundImageLeftValue = () => {
-  console.log('a')
   const groundImageWidth = getGroundConatinerWidth()
   const firstGroundImagePosition = getPropertyValue(groundImage1, 'left')
   const newImagePosition = groundImageWidth - (firstGroundImagePosition - firstGroundImagePosition * 2) - 2
