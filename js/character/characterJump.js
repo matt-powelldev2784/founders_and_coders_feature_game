9c317f1a376663d3394aaace9df32e2364c96b7e
Matthew Powell <matt.powell2784@gmail.com>
2023-01-03 18:25:52 +0000

    character jump initial commit

--- js/character/characterJump.js
+++ js/character/characterJump.js
@@ -1,10 +1,12 @@
-import { getPropertyValue, setPropertyValue } from '../helpers/updateProperties.js  '
+import { getPropertyValue, setPropertyValue } from '../helpers/updateProperties.js'
+import { SPEED } from '../globalVariables.js'
 
 export const characterJump = () => {
   const character = document.getElementById('main__char')
   const bottomPosition = '23.5%'
 
   const characterCurrentBottomPosition = getPropertyValue(character, 'bottom')
-  const characterNewBottomPosition = `${characterCurrentBottomPosition - delta * SPEED + 5}%`
+  console.log('characterCurrentBottomPosition', characterCurrentBottomPosition)
+  const characterNewBottomPosition = `${characterCurrentBottomPosition + 20}px`
   setPropertyValue(character, 'bottom', characterNewBottomPosition)
 }
