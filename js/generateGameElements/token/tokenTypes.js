59a97ddfd176a2abbe520a691c40f6e186ad2f90
Matthew Powell <matt.powell2784@gmail.com>
2023-02-01 17:32:07 +0000

    images converted to png

--- js/generateGameElements/token/tokenTypes.js
+++ js/generateGameElements/token/tokenTypes.js
@@ -3,31 +3,31 @@ import { generateRandomNumber } from '../../helpers/generateRandomNumber.js';
 export const tokenTypes = [
   {
     key: 0,
-    img: './img/tokens/html_icon_8bit.svg',
+    img: './img/tokens/html_icon_8bit.png',
     text: 'HTML',
     points: 100,
   },
   {
     key: 1,
-    img: './img/tokens/css_logo_8bit.svg',
+    img: './img/tokens/css_logo_8bit.png',
     text: 'CSS',
     points: 100,
   },
   {
     key: 2,
-    img: './img/tokens/js_logo_8bit.svg',
+    img: './img/tokens/js_logo_8bit.png',
     text: 'JAVASCRIPT',
     points: 300,
   },
   {
     key: 3,
-    img: './img/tokens/react_logo_8bit.svg',
+    img: './img/tokens/react_logo_8bit.png',
     text: 'REACT',
     points: 500,
   },
   {
     key: 4,
-    img: './img/tokens/node_js_8bit.svg',
+    img: './img/tokens/node_js_8bit.png',
     text: 'NODEJS',
     points: 1000,
   },
